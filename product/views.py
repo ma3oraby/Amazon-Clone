ec7c11770b2bb5e422de78f3f0cf7a3a0185dbf7
oraby <na3oraby@gmail.com>
2023-05-04 15:22:02 +0300

    add review

--- product/views.py
+++ product/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Product
 from django.views.generic import ListView , DetailView
+from .forms import ProductReviewForm
 
 class ProductList(ListView):
     model = Product
 
 class ProductDetail(DetailView):
     model = Product 
+
+def add_review (request,slug):
+    product = Product.objects.get(slug=slug)
+    if request.method == 'POST' : 
+        form = ProductReviewForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.user = request.user
+            myform.product = product 
+            myform.save()
+    return redirect(f'/products/{product.slug}')
+
