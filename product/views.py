9d37b4b0426e41adb47202085cd71f2221e55d1d
oraby <na3oraby@gmail.com>
2023-05-07 11:35:02 +0300

    setup base template

--- product/views.py
+++ product/views.py
@@ -9,14 +9,14 @@ class ProductList(ListView):
 class ProductDetail(DetailView):
     model = Product 
 
-def add_review (request,slug):
+def add_review(request,slug):
     product = Product.objects.get(slug=slug)
-    if request.method == 'POST' : 
+    if request.method == 'POST':
         form = ProductReviewForm(request.POST)
         if form.is_valid():
             myform = form.save(commit=False)
             myform.user = request.user
-            myform.product = product 
+            myform.product = product
             myform.save()
     return redirect(f'/products/{product.slug}')
 
