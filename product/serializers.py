d7f977ee2158a2bf50963927cf029f3da7f0822a
Oraby <ma3oraby@gmail.com>
2023-05-30 14:14:55 +0300

    add extra field to any api

--- product/serializers.py
+++ product/serializers.py
@@ -5,7 +5,7 @@ from .models import Product , Brand , ProductReview , ProductImages
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta :
         model = ProductImages 
-        fields = '__all__'
+        fields = ['image']
 
 
 class BrandSerializer (serializers.ModelSerializer):
@@ -24,9 +24,13 @@ class ProductReviewSerializer(serializers.ModelSerializer):
 class ProductListSerializers (serializers.ModelSerializer):
     #brand = BrandSerializer() ##all brand detail 
     brand = serializers.StringRelatedField() ###brand name only 
+    price_with_tax = serializers.SerializerMethodField()
     class Meta :
         model = Product
-        fields = ['image']
+        fields = '__all__'
+
+    def get_price_with_tax (self,product):
+        return product.price*1.1
 
 
 class ProductDetailSerializers (serializers.ModelSerializer):
